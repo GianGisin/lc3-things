cf21cd4b0a71fca4ffd2142f46abd4a499f4408e
Gian Gisin <ggisin@student.ethz.ch>
2026-04-25 16:45:23 +0200

    create instruction class

---
+++ lc3em/src/main/java/ch/giang/lc3em/Instruction.java
@@ -0,0 +1,42 @@
+package ch.giang.lc3em;
+
+public class Instruction {
+    public opCode op;
+    private String bits;
+
+    public Instruction(String bits) {
+        assert bits.length() == 16;
+        this.bits = bits;
+        op = opCodeFromBits(bits.substring(0, 4));
+    }
+
+    public boolean bit(int index) {
+        return bits.charAt(15 - index) == '1';
+    }
+
+    static opCode opCodeFromBits(String bits) {
+        // FIXME: opcode.values takes O(n) because it copies the array
+        return opCode.values()[Integer.parseInt(bits, 2)];
+    }
+
+}
+
+enum opCode {
+    BR, // 0000 0
+    ADD, // 0001 1
+    LD, // 0010 2
+    ST, // 0011 3
+    JSR, // 0100 4
+    AND, // 0101 5
+    LDR, // 0110 6
+    STR, // 0111 7
+    RTI, // 1000 8
+    NOT, // 1001 9
+    LDI, // 1010 10
+    STI, // 1011 11
+    RET, // 1100 12
+    ILLEGAL, // 1101 13
+    LEA, // 1110 14
+    TRAP, // 1111 15
+
+}
