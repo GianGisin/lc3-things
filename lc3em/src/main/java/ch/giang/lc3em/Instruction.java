e3e3262ece109ad4884f8968754c40836eaf89ae
Gian Gisin <ggisin@student.ethz.ch>
2026-04-25 21:04:52 +0200

    sub instruction api

--- lc3em/src/main/java/ch/giang/lc3em/Instruction.java
+++ lc3em/src/main/java/ch/giang/lc3em/Instruction.java
@@ -10,6 +10,62 @@ public class Instruction {
         op = opCodeFromBits(bits.substring(0, 4));
     }
 
+    public int PCOffset9() {
+        return Integer.parseInt(bits.substring(7), 2);
+    }
+
+    public int PCOffset11() {
+        return Integer.parseInt(bits.substring(5), 2);
+    }
+
+    public int Offset6() {
+        return Integer.parseInt(bits.substring(10), 2);
+    }
+
+    public int Trapvect8() {
+        return Integer.parseInt(bits.substring(8), 2);
+    }
+
+    public short Imm5() {
+        return Short.parseShort(bits.substring(11), 2);
+    }
+
+    public short SR1() {
+        return Short.parseShort(bits.substring(7, 10), 2);
+    }
+
+    public short BaseR() {
+        return SR1();
+    }
+
+    public short SR2() {
+        return Short.parseShort(bits.substring(13, 16), 2);
+    }
+
+    public short DR() {
+        return Short.parseShort(bits.substring(4, 7));
+    }
+
+    public short SR() {
+        return DR();
+    }
+
+    public boolean immBit() {
+        return bit(5);
+    }
+
+    public boolean n() {
+        return bit(11);
+    }
+
+    public boolean z() {
+        return bit(10);
+    }
+
+    public boolean p() {
+        return bit(9);
+    }
+
     public boolean bit(int index) {
         return bits.charAt(15 - index) == '1';
     }
