3203965697fe19ef3e844f4b8bafe9680e8d5976
Gian Gisin <ggisin@student.ethz.ch>
2026-04-27 20:38:40 +0200

    add sign extend for offsets and immediates

--- lc3em/src/main/java/ch/giang/lc3em/Instruction.java
+++ lc3em/src/main/java/ch/giang/lc3em/Instruction.java
@@ -10,38 +10,41 @@ public class Instruction {
         op = opCodeFromBits(bits.substring(0, 4));
     }
 
+    // offset literals
     public int PCOffset9() {
-        return Integer.parseInt(bits.substring(7), 2);
+        return CPU.sext(bits.substring(7));
     }
 
     public int PCOffset11() {
-        return Integer.parseInt(bits.substring(5), 2);
+        return CPU.sext(bits.substring(5));
     }
 
     public int Offset6() {
-        return Integer.parseInt(bits.substring(10), 2);
+        return CPU.sext(bits.substring(10));
     }
 
     public int Trapvect8() {
         return Integer.parseInt(bits.substring(8), 2);
     }
 
+    // immediate for and + add
     public short Imm5() {
-        return Short.parseShort(bits.substring(11), 2);
+        return CPU.sext(bits.substring(11));
     }
 
     public short SR1() {
         return Short.parseShort(bits.substring(7, 10), 2);
     }
 
-    public short BaseR() {
-        return SR1();
-    }
-
     public short SR2() {
         return Short.parseShort(bits.substring(13, 16), 2);
     }
 
+    public short BaseR() {
+        return SR1();
+    }
+
+    // destination register for operate instructions
     public short DR() {
         return Short.parseShort(bits.substring(4, 7), 2);
     }
@@ -54,6 +57,7 @@ public class Instruction {
         return bit(5);
     }
 
+    // condition codes
     public boolean n() {
         return bit(11);
     }
@@ -66,14 +70,14 @@ public class Instruction {
         return bit(9);
     }
 
-    public boolean jumpMode(){
+    // returns true if offset mode, else base register mode
+    public boolean jumpMode() {
         return bit(11);
     }
 
     public boolean bit(int index) {
         return bits.charAt(15 - index) == '1';
     }
-
 
     static opCode opCodeFromBits(String bits) {
         // FIXME: opcode.values takes O(n) because it copies the array
